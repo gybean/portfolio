6dfcd5279ca292a43306df6f6798660621c370ef
gybean <kimyb0063@naver.com>
2025-05-28 15:52:18 +0900

    2025.05.28

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
     //로딩페이지
     $('.loading_wrap').addClass('on');
 
-    setTimeout(function(){
+    setTimeout(function () {
         $('#loading').slideUp(700);
     }, 3500);
 
@@ -22,5 +22,35 @@ $(function () {
 
 
 
-    
+    //메인타이틀
+    //gsap
+    gsap.registerPlugin(CustomEase, CustomBounce, SplitText);
+
+    CustomBounce.create("myBounce", {
+        strength: 0.6,
+        squash: 1.5,
+        squashID: "myBounce-squash"
+    });
+
+    const splitTxt = new SplitText("#txt").chars;
+
+    const tl = gsap.timeline({ defaults: { duration: 1.5, stagger: { amount: 0.1, ease: 'sine.in' } } })
+        .from(splitTxt, {
+            duration: 0.6,
+            opacity: 0,
+            ease: "power1.inOut"
+        }, 0)
+        .from(splitTxt, {
+            y: -350,
+            ease: "myBounce"
+        }, 0)
+        .to(splitTxt, {
+            scaleX: 1.8,
+            scaleY: 0.7,
+            rotate: (i) => 15 - 30 * Math.random(),
+            ease: "myBounce-squash",
+            transformOrigin: "50% 100%"
+        }, 0)
+
+    window.onclick = () => tl.play(0)
 });
