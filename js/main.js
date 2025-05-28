dda8ffa1d85b82958d6eee9fab6e5cb7e9703df2
gybean <kimyb0063@naver.com>
2025-05-28 12:03:11 +0900

    2025.05.28

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 $(function () {
+    //로딩페이지
+    $('.loading_wrap').addClass('on');
+
+    setTimeout(function(){
+        $('#loading').slideUp(700);
+    }, 3500);
+
+
+
+    //풀페이지
     var myFullpage = new fullpage('#fullpage', {
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        sectionsColor: ['#fff'],
         anchors: ['firstPage', 'secondPage', '3rdPage'],
-        navigation: true,
+        navigation: false,
         navigationPosition: 'left',
         navigationTooltips: ['Page 1', 'Page 2', 'Any text!'],
         showActiveTooltip: true,
         menu: '#menu',
         responsiveWidth: 900,
     });
+
+
+
+    
 });
